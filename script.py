e66a09919d3f6f6beec72ca610377f005395310d
Laure-Anne Caniot <laureacaniot@gmail.com>
2021-03-24 16:19:07 +0100

    script.py

--- script.py
+++ script.py
@@ -1,24 +1,13 @@
-<<<<<<< HEAD
-
-m book import Book
-=======
-#!/usr/bin/env python3
+!/usr/bin/env python3
 from book import Book
->>>>>>> 65778b39dfaaae5298ae910bc649f99faa147f6a
 def main():
     book = Book("TEST")
-    book.insert_buy(10, 10.0)
-    book.insert_sell(120, 12.0)
-    book.insert_buy(5, 10.0)
-    book.insert_buy(2, 11.0)
-    book.insert_sell(1, 10.0)
-    book.insert_sell(10, 10.0)
-<<<<<<< HEAD
-if __name__== "__main__":
-=======
+    book.insert_order(10, 10.0) #by default we insert a buy order
+    book.insert_order(120, 12.0, False) #here we insert a sell order
+    book.insert_order(5, 10.0)
+    book.insert_order(2, 11.0)
+    book.insert_order(1, 10.0, False)
+    book.insert_order(10, 10.0, False)
 if __name__ == "__main__":
->>>>>>> 65778b39dfaaae5298ae910bc649f99faa147f6a
     main()
-
-
 
